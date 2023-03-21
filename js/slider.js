67d0e376e2448a5a5bdf9749a533ff42e9123a2f
Olena-Rudnieva <rudnievao@gmail.com>
2023-03-21 02:15:59 +0200

    Update styles

--- js/slider.js
+++ js/slider.js
@@ -1,7 +1,43 @@
-$(function(){
-    $('.slider-wrap').slick({
-        arrows: false,
-        dots: true,
+// $(function(){
+//     $('.slider-wrap').slick({
+//         arrows: false,
+//         dots: true,
     
-    });
+//     });
+// });
+
+$('.slider-wrap').slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+   
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
 });
+
+
+
+
+
+
